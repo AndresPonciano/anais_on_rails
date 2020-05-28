38dda3b832b0b5443c62fdefbb2ce7b33aed098c
Andres Ponciano <andres.ponciano123@gmail.com>
2020-05-28 18:12:49 -0400

    customized bulma and started main page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,31 @@
 class ProductsController < ApplicationController
 
     def index
-    
+        @products = Product.all.order("created_at DESC")
     end
 
     def new
+        @product = Product.new
     end
 
     def create
+        @Product = Product.new(product_params)
+
+        if @Product.save
+            redirect_to @Product
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @product = Product.find(params[:id])
+    end
+
+    private 
+
+    def product_params
+        params.require(:product).permit(:title, :price)
     end
 
 end
